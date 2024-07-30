b3adf0f2cf1ea09a886daa995dcd7f079f7243d3
baccrie <bakarerilwan1@gmail.com>
2024-07-30 20:30:12 +0100

    Error Handling

--- src/controller/errorHandler.ts
+++ src/controller/errorHandler.ts
@@ -3,11 +3,26 @@ import { Request, Response, NextFunction } from 'express';
 import ICustomError from '../interface/error.js';
 
 export default function errorHandler(error: ICustomError, req: Request, res: Response, next: NextFunction) {
-  const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  const message = error.message || 'Something went wrong';
+  let statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = error.message || 'Something went wrong';
 
+  if (error.name === 'ValidationError') {
+    message = Object.values(error.errors)
+      .map((item) => item.message)
+      .join(',');
+  statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (error.code && error.code === 11000) {
+    message = `Duplicate value entered for ${Object.keys(
+      error.keyValue
+    )} field, please choose another value`;
+    statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  const start: Boolean = `${statusCode}`.startsWith('4')
   res.status(statusCode).json({
-    status: 'error',
+    status: `${start ? 'error' : 'fail'}`,
     msg: message
   });
 }
--- src/interface/error.ts
+++ src/interface/error.ts
@@ -1,4 +1,9 @@
 export default interface ICustomError extends Error {
+  errors(errors: any): unknown;
+  path: any;
+  title: string;
+  keyValue(keyValue: any): unknown;
+  code?: number;
   statusCode?: number;
   msg?: string;
 }
