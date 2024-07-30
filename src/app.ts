b95448589467f7e7e7da601aa7d0962d309dca77
baccrie <bakarerilwan1@gmail.com>
2024-07-30 14:19:32 +0100

    setting up PORT and env vars

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { StatusCodes } from 'http-status-codes'
 
+dotenv.config({path: './.env'})
 const app = express()
 
-const PORT: any = 8080
+const PORT: any = process.env.PORT || 8000
 
 app.listen((PORT: any) => console.log(`server is running on port ${PORT}`))
