e01f1b94f7ba246e25fdb5a8640389feafac3e72
baccrie <bakarerilwan1@gmail.com>
2024-07-31 15:15:59 +0100

    Adding security packages

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import { StatusCodes } from 'http-status-codes'
 import fileUpload from 'express-fileupload';
 import swaggerUi from 'swagger-ui-express';
 import openApiDocumentation from './docs.json'
-
-
+import cors from 'cors'
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from 'helmet';
 
 // self modules
 import connectDB from './db/connect'
@@ -14,12 +15,17 @@ import router from './router/book'
 import notFound from './controller/notFound'
 import errorHandler from './controller/errorHandler'
 
-
 dotenv.config({path: './.env'})
 const app = express()
 
-
 // built-in middlewares
+
+// security packages
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+
+// others
 app.use(express.json());
 app.use(express.static('./public'))
 
